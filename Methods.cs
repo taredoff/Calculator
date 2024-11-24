385ec85a8ed61f1b4bca787738612188afc514ec
tiredBW <avilovalesha@gmail.com>
2024-11-24 16:51:32 +0500

    initial commit

---
+++ Methods.cs
@@ -0,0 +1,23 @@
+namespace program
+{
+    class Methods
+    { 
+        public static double answer = 0;
+        public static void Add(double a, double b)
+        {
+            answer =  a + b;
+        }
+        public static void Sub(double a, double b)
+        {
+            answer = a - b;
+        }
+        public static void Mult(double a, double b)
+        {
+            answer = a * b;
+        }
+        public static void Div(double a, double b)
+        {
+            answer = a / b;
+        }
+    }
+}
